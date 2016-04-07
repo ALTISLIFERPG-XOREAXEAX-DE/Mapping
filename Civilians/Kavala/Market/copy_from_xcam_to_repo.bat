55740eb27a65d6dd8cd3e2cc0fa566b300ea45f9
Dr. Ignaz M <9mmfullclip@gmail.com>
2016-04-07 08:57:36 +0200

    yo

--- Civilians/Kavala/Market/copy_from_xcam_to_repo.bat
+++ Civilians/Kavala/Market/copy_from_xcam_to_repo.bat
@@ -2,8 +2,8 @@
 
 copy "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\_xCamProjects.ini" .
 
-copy "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\Altis_Kavala_Market.ini" .
+copy "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\Altis_Kavala_Markt2.ini" .
 
-copy "D:\Steam\steamapps\common\Arma 3\mission_Altis_Kavala_Market.sqm" .
+copy "D:\Steam\steamapps\common\Arma 3\mission_Altis_Kavala_Markt2.sqm" .
 
 del "D:\Steam\steamapps\common\Arma 3"\mission_*.sqm
