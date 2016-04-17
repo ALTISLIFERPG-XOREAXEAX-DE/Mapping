9f437cd4872a6b8192147806e17753ec18923058
Dr. Ignaz M <9mmfullclip@gmail.com>
2016-04-17 16:20:15 +0200

    yo

---
+++ Civilians/Kavala/Parking/copy_from_xcam_to_repo.bat
@@ -0,0 +1,9 @@
+@echo off
+
+copy "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\_xCamProjects.ini" .
+
+copy "D:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\Altis_Kavala_Parking.ini" .
+
+copy "D:\Steam\steamapps\common\Arma 3\mission_Altis_Kavala_Parking.sqm" .
+
+del "D:\Steam\steamapps\common\Arma 3"\mission_*.sqm
---
+++ Clans/DKA/Base/copy_from_xcam_to_repo.bat
@@ -0,0 +1,9 @@
+@echo off
+
+copy "d:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\_xCamProjects.ini" _xcamProjects.ini
+
+copy "d:\Steam\steamapps\common\Arma 3\@inidbi\db\xCam\Library_Pro\Altis_Atsalis.ini" Altis_Atsalis.ini
+
+copy "d:\Steam\steamapps\common\Arma 3\mission_Altis_Atsalis.sqm" .
+
+del "D:\Steam\steamapps\common\Arma 3"\mission_*.sqm
